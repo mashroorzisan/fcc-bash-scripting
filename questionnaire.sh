a00c43f15113d20ac63e4e6f3b9ca9dc2505f0c9
zisan <mashroorzisan@gmail.com>
2024-04-01 01:48:43 +0600

    countdown.sh

--- questionnaire.sh
+++ questionnaire.sh
@@ -3,6 +3,7 @@ echo -e "\n~~ Questionnaire ~~\n"
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
 QUESTION3="What's your favorite coding website?"
+
 echo $QUESTION1
 read NAME
 echo Hello $NAME.
